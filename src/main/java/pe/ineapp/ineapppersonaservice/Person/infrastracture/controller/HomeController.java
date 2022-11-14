d1675ca1c177a4547f4e5b59cf78192277269042
Pablo La Plata <p.laplata.m@gmail.com>
2022-11-13 23:18:14 -0500

    home anadido

---
+++ src/main/java/pe/ineapp/ineapppersonaservice/Person/infrastracture/controller/HomeController.java
@@ -0,0 +1,16 @@
+package pe.ineapp.ineapppersonaservice.Person.infrastracture.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class HomeController {
+
+    @RequestMapping("")
+    public String Index(@RequestParam(defaultValue = "Invitado") String param){
+        return "Bienvenido".concat(param);
+    }
+}
